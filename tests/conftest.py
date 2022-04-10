4fd47a542e20025e4018654eb311168d3a8ccebe
SirVsevolod <19vsevolod97@gmail.com>
2022-04-11 01:15:03 +0300

    +Docker

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,40 @@
+import time
+
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+# @pytest.fixture(scope="function")
+# def browser():
+#     print("\nstart chrome browser for test..")
+#     browser = webdriver.Chrome()
+#     yield browser
+#     print("\nquit browser..")
+#     browser.quit()
+
+capabilities = {
+    "browserName": "chrome",
+    "--nosandbox": True,
+    "browserVersion": "99.0",
+    "screenResolution": "1920x1080x24",
+    "selenoid:options": {
+        "enableVNC": True,
+        "enableVideo": False,
+    }
+}
+#
+#
 
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart chrome browser for test..")
-    browser = webdriver.Chrome(executable_path=r'C:\chromedriver\chromedriver.exe')
+    browser = webdriver.Remote(
+        command_executor="http://selenoid:4444/wd/hub",
+        desired_capabilities=capabilities
+    )
     yield browser
     print("\nquit browser..")
     browser.quit()
+
+
+
